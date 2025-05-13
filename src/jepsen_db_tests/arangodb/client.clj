64e93b00ac0d5daf1a2538f29e20944640214b02
sonyjain09 <63935719+sonyjain09@users.noreply.github.com>
2025-05-13 14:46:04 -0500

    Update client.clj

--- src/jepsen_db_tests/arangodb/client.clj
+++ src/jepsen_db_tests/arangodb/client.clj
@@ -1,1 +1,19 @@
+(ns jepsen-db-tests.arangodb.client
+  (:require [jepsen.client :as client]))
 
+(defrecord ArangoClient []
+  client/Client
+  (setup!    [_ test node]
+    ;; Initialize HTTP driver or arangosh client
+    )
+  (invoke!   [_ test op]
+    (case (:f op)
+      :read  (assoc op :type :ok :value (str "value-of-" (:value op)))
+      :write (assoc op :type :ok)
+      op))
+  (teardown! [_ test]
+    ;; Close HTTP client
+    ))
+
+(defn client [] (->ArangoClient))
+(defn db     [] nil)
