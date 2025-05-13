583c8f62c02a1a8ed9051bc3a1ae44ef32bb7f64
Saichetan Aitha <saiaitha99@gmail.com>
2025-05-13 16:17:41 -0500

    Update foundationdb-setup.sh

--- resources/scripts/foundationdb-setup.sh
+++ resources/scripts/foundationdb-setup.sh
@@ -8,7 +8,12 @@ yum install -y https://repos.foundationdb.org/fdb/releases/7.1.19/redhat7/founda
 # Example 5-node cluster file (adjust addresses as needed)
 cat <<EOF >/etc/foundationdb/fdb.cluster
 cluster fdb-cluster-00
-
+token your-very-long-token-string
+127.0.0.1:4500
+127.0.0.1:4501
+127.0.0.1:4502
+127.0.0.1:4503
+127.0.0.1:4504
 EOF
 
 systemctl enable foundationdb
