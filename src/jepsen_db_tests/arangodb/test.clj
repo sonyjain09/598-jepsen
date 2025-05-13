e5c3c149dea374c877f20679d24465b773bf104a
sonyjain09 <63935719+sonyjain09@users.noreply.github.com>
2025-05-13 14:46:30 -0500

    Update test.clj

--- src/jepsen_db_tests/arangodb/test.clj
+++ src/jepsen_db_tests/arangodb/test.clj
@@ -1,1 +1,19 @@
+(ns jepsen-db-tests.arangodb.test
+  (:require [jepsen.core                       :as jepsen]
+            [jepsen-db-tests.arangodb.client   :as arango-client]
+            [jepsen-db-tests.arangodb.nemesis  :as arango-nem]
+            [jepsen-db-tests.utils.generator   :as gen]
+            [jepsen-db-tests.utils.checker     :as checker]))
 
+(defn arangodb-test [opts]
+  (merge {:name      "arangodb-multi-nemesis"
+          :os        jepsen.os.debian/os
+          :db        (arango-client/db)
+          :client    (arango-client/client)
+          :nemesis   (arango-nem/nemesis)
+          :generator (gen/workload :balanced)
+          :checker   (checker/strong)}
+         opts))
+
+(defn run-test [opts]
+  (jepsen/run! (arangodb-test opts)))
