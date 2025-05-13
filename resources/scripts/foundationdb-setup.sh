954252686fad5fceae1900caec2e3225b9539b9e
sonyjain09 <63935719+sonyjain09@users.noreply.github.com>
2025-05-13 14:42:16 -0500

    Update foundationdb-setup.sh

--- resources/scripts/foundationdb-setup.sh
+++ resources/scripts/foundationdb-setup.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
+set -eux
 
+# Install FoundationDB
+yum install -y https://repos.foundationdb.org/fdb/releases/7.1.19/redhat7/foundationdb-clients-7.1.19-1.x86_64.rpm \
+               https://repos.foundationdb.org/fdb/releases/7.1.19/redhat7/foundationdb-server-7.1.19-1.x86_64.rpm
+
+# Example 5-node cluster file (adjust addresses as needed)
+cat <<EOF >/etc/foundationdb/fdb.cluster
+cluster fdb-cluster-00
+# add nodes
+EOF
+
+systemctl enable foundationdb
+systemctl start  foundationdb
