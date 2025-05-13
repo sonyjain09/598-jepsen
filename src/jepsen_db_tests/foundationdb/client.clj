f8edb9b3525dae4049aa24cf30b2f41b1b64ff37
sonyjain09 <63935719+sonyjain09@users.noreply.github.com>
2025-05-13 14:46:47 -0500

    Update client.clj

--- src/jepsen_db_tests/foundationdb/client.clj
+++ src/jepsen_db_tests/foundationdb/client.clj
@@ -1,1 +1,19 @@
+(ns jepsen-db-tests.foundationdb.client
+  (:require [jepsen.client :as client]))
 
+(defrecord FDBClient []
+  client/Client
+  (setup!    [_ test node]
+    ;; Initialize FDB cluster connection
+    )
+  (invoke!   [_ test op]
+    (case (:f op)
+      :read  (assoc op :type :ok :value (str "value-of-" (:value op)))
+      :write (assoc op :type :ok)
+      op))
+  (teardown! [_ test]
+    ;; Close FDB connection
+    ))
+
+(defn client [] (->FDBClient))
+(defun db     [] nil)
