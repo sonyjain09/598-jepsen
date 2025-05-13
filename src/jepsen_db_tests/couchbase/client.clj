a210342f0c49cb42af349366752408946a25e6ed
sonyjain09 <63935719+sonyjain09@users.noreply.github.com>
2025-05-13 14:45:16 -0500

    Update client.clj

--- src/jepsen_db_tests/couchbase/client.clj
+++ src/jepsen_db_tests/couchbase/client.clj
@@ -1,1 +1,19 @@
+(ns jepsen-db-tests.couchbase.client
+  (:require [jepsen.client :as client]))
 
+(defrecord CouchbaseClient [bucket]
+  client/Client
+  (setup!    [_ test node]
+    ;; Initialize SDK connection to Couchbase node
+    )
+  (invoke!   [_ test op]
+    (case (:f op)
+      :read  (assoc op :type :ok :value (str "value-of-" (:value op)))
+      :write (assoc op :type :ok)
+      op))
+  (teardown! [_ test]
+    ;; Close SDK connection
+    ))
+
+(defn client [] (->CouchbaseClient "jepsen"))
+(defn db     [] nil)  ;; No separate DB process here
