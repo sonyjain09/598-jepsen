0f1d2a59b944da34b99bba499933d337f98eda7e
sonyjain09 <63935719+sonyjain09@users.noreply.github.com>
2025-05-13 14:41:05 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,1 +1,7 @@
-
+(defproject jepsen-db-tests "0.1.0-SNAPSHOT"
+  :description "Jepsen experiments (Couchbase, ArangoDB, FoundationDB) with multi-nemesis sequencing"
+  :dependencies [[org.clojure/clojure       "1.11.1"]
+                 [jepsen                    "0.3.4"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.clojure/tools.cli     "1.0.214"]]
+  :main jepsen-db-tests.core)
